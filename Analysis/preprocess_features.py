17bded621be25f4e16acfb6929ce4441c03442f9
Adam Richard-Bollans <mm15alrb@leeds.ac.uk>
2021-03-26 10:21:07 +0000

    Move Feature class to data_import.py

--- Analysis/preprocess_features.py
+++ Analysis/preprocess_features.py
@@ -2,171 +2,20 @@
 Contains Features class which, for a given study, reads file of extracted features, removes some features,
 gets average location control, standardises values and outputs
 """
+from data_import import StudyInfo
 
-import pandas as pd
 
-
-class Features:
+def process_all_features(study):
     """Summary
-    Provides methods for standardising feature values and converting between raw values and standardised values.
-    Attributes:
-        dataset (TYPE): Description
-        features_to_remove (TYPE): Description
-        human_readable_path (TYPE): Description
-        means (TYPE): Description
-        output_path (TYPE): Description
-        input_feature_csv (TYPE): Description
-        stds (TYPE): Description
     """
 
-    # Features given in relation.csv that aren't needed for analysis
-    features_to_remove = ["size_ratio", "horizontal_distance_normalized", "shortest_distance_normalized",
-                          "location_control_-x", "location_control_-z", "location_control_x",
-                          "location_control_z", "figure_volume", "ground_volume",
-                          "horizontal_projection_overlap_proportion"]
-    # Number of columns at start of each row which do not contain numeric values.
-    number_of_non_value_columns = 3
+    f = study.feature_processor
+    nd = f.standardise_values()
+    f.write_new(nd)
+    f.write_mean_std()
 
-    def __init__(self, input_feature_csv,output_feature_csv,output_mean_csv,output_std_csv,output_human_readable_feature_csv):
-        """Summary
-        
-        Args:
-            study (TYPE): Description
-        """
 
-        # Get path for calculated features
-        self.input_feature_csv = input_feature_csv
-        self.output_path = output_feature_csv
-        self.means_output_path = output_mean_csv
-        self.std_output_path = output_std_csv
-        self.human_readable_path = output_human_readable_feature_csv
+if __name__ == '__main__':
 
-        self.dataset = pd.read_csv(self.input_feature_csv)
-        # Remove values where fig=ground
-
-        self.dataset = self.dataset[self.dataset.Figure != self.dataset.Ground]
-
-        # Calculate location control and append column
-        location_control_parts = ["location_control_x", "location_control_-x", "location_control_-z",
-                                  "location_control_z"]
-
-        self.dataset["location_control"] = self.dataset[location_control_parts].sum(axis=1) / 4
-
-        # Remove unwanted columns
-        self.dataset = self.dataset.drop(self.features_to_remove, axis=1)
-        # Output cleaned dataset
-        self.dataset.to_csv(self.human_readable_path, index=False)
-        # Calculate means and standard deviations
-        # Can be used to convert standardised values back to real values
-        # Note that different pandas versions will give slightly different float values, which has knock on effects
-        # E.g. some configurations are then classed as possible polyseme instances which weren't previously
-        # Here we use pandas version 1.0.3
-        self.means = self.dataset.iloc[:, self.number_of_non_value_columns:].mean()
-        self.stds = self.dataset.iloc[:, self.number_of_non_value_columns:].std()
-
-    def standardise_values(self):
-        """Summary
-        
-        Returns:
-            TYPE: Description
-        """
-        new_dataframe = self.dataset.copy()
-
-        new_dataframe.iloc[:, self.number_of_non_value_columns:] = new_dataframe.iloc[:,
-                                                                   self.number_of_non_value_columns:] - self.means
-        new_dataframe.iloc[:, self.number_of_non_value_columns:] = new_dataframe.iloc[:,
-                                                                   self.number_of_non_value_columns:] / (self.stds)
-
-        return new_dataframe
-
-    def write_new(self, dataframe):
-        """Summary
-        
-        Args:
-            dataframe (TYPE): Description
-        """
-        dataframe.to_csv(self.output_path, index=False)
-
-    def write_mean_std(self):
-        """Summary
-        """
-        self.means.to_csv(self.means_output_path)
-        self.stds.to_csv(self.std_output_path)
-
-    def convert_standardised_value_to_normal(self, feature, value):
-        """Summary
-        
-        Args:
-            feature (TYPE): Description
-            value (TYPE): Description
-        
-        Returns:
-            TYPE: Description
-        """
-        mean = self.means[feature]
-        std = self.stds[feature]
-
-        x = (value * std) + mean
-
-        return x
-
-    def convert_normal_value_to_standardised(self, feature, value):
-        """Summary
-        
-        Args:
-            feature (TYPE): Description
-            value (TYPE): Description
-        
-        Returns:
-            TYPE: Description
-        """
-        mean = self.means[feature]
-        std = self.stds[feature]
-
-        x = (value - mean) / std
-
-        return x
-
-    def get_max_value(self, feature):
-        """Summary
-        
-        Args:
-            feature (TYPE): Description
-        
-        Returns:
-            TYPE: Description
-        """
-        m = self.dataset[feature].max()
-        return m
-
-    def get_min_value(self, feature):
-        """Summary
-        
-        Args:
-            feature (TYPE): Description
-        
-        Returns:
-            TYPE: Description
-        """
-        m = self.dataset[feature].min()
-        return m
-
-    def convert_standard_df_to_normal(self, df):
-        """Summary
-        
-        Args:
-            df (TYPE): Description
-        
-        Returns:
-            TYPE: Description
-        """
-        # Need to be careful assigning new dataframe variables this way
-        # As it returns a reference to the initial dataframe UNLESS copy() is used
-        new_dataframe = df.copy()
-        for column in new_dataframe:
-            mean = self.means[column]
-            std = self.stds[column]
-
-            new_dataframe[column] = (new_dataframe[column] * std) + mean
-
-        return new_dataframe
+    process_all_features(StudyInfo("2019 study"))
+    process_all_features(StudyInfo("2020 study"))
